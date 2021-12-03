b1ad441555228986bc464e4980ab301004f48818
Adrian Pohl <pohl@hbz-nrw.de>
2021-12-03 14:50:05 +0100

    Remove non-JSON-Schema conformant key, simplify test

--- test.sh
+++ test.sh
@@ -2,18 +2,8 @@
 
 set -e
 
-# Test JSON examples against schema
-for version in "draft"; do
-	echo "Testing version: $version"
-
-#	for ` $version/schemas/schema.json`; do
-#		echo "Testing $schema_file"
-#		schema_name=$(echo "$schema_file" | cut -f 1 -d '.')
-		ajv test -s $version/schemas/schema.json -r "$version/schemas/!(schema).json" -d "$version/examples/valid/*.json" --valid
-		ajv test -s $version/schemas/schema.json -r "$version/schemas/!(schema).json" -d "$version/examples/invalid/*.json" --invalid
-#		echo ""
-#	done
-done
+		ajv test -s draft/schemas/schema.json -r "draft/schemas/!(schema).json" -d "draft/examples/valid/*.json" -c ajv-formats --valid
+		ajv test -s draft/schemas/schema.json -r "draft/schemas/!(schema).json" -d "draft/examples/invalid/*.json" -c ajv-formats --invalid
 
 if [ $? -eq 0 ]
 then
