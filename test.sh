c6c32b9790bc206b7c8a104026a5a4e121dfaa4f
Stephan Kulla <git.mail@kulla.me>
2022-01-21 15:58:06 +0100

    Refactor test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-		ajv test -s draft/schemas/schema.json -r "draft/schemas/!(schema).json" -d "draft/examples/valid/*.json" -c ajv-formats --valid
-		ajv test -s draft/schemas/schema.json -r "draft/schemas/!(schema).json" -d "draft/examples/invalid/*.json" -c ajv-formats --invalid
+run_ajv () {
+  ajv test -s draft/schemas/schema.json -r "draft/schemas/!(schema).json" \
+           -d "$1" -c ajv-formats $2
+}
 
-if [ $? -eq 0 ]
-then
-  echo -e "All tests \033[0;32mPASSED\033[0m\n"
-fi
+run_ajv "draft/examples/valid/*.json" --valid
+run_ajv "draft/examples/invalid/*.json" --invalid
+
+echo "All tests \033[0;32mPASSED\033[0m\n"
